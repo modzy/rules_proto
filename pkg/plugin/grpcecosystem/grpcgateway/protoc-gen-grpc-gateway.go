941e2e4c2bf6a6862291b2bc65de8d18f906415f
Nathan Mellis <nathan@mellis.us>
2022-11-29 07:18:37 -0800

    gRPC Gateway Golang output now properly inclued as part of compiled sources

--- pkg/plugin/grpcecosystem/grpcgateway/protoc-gen-grpc-gateway.go
+++ pkg/plugin/grpcecosystem/grpcgateway/protoc-gen-grpc-gateway.go
@@ -1,10 +1,12 @@
 package grpcgateway
 
 import (
-	"path"
-
 	"github.com/bazelbuild/bazel-gazelle/label"
+	"github.com/bazelbuild/bazel-gazelle/rule"
+	"github.com/stackb/rules_proto/pkg/plugin/golang/protobuf"
 	"github.com/stackb/rules_proto/pkg/protoc"
+	"path"
+	"strings"
 )
 
 func init() {
@@ -24,33 +26,57 @@ func (p *protocGenGrpcGatewayPlugin) Configure(ctx *protoc.PluginContext) *proto
 	if !p.shouldApply(ctx.ProtoLibrary) {
 		return nil
 	}
+	options := ctx.PluginConfig.GetOptions()
+	mappings, _ := protobuf.GetImportMappings(options)
 	return &protoc.PluginConfiguration{
 		Label:   label.New("build_stack_rules_proto", "plugin/grpc-ecosystem/grpc-gateway", "protoc-gen-grpc-gateway"),
-		Outputs: p.outputs(ctx.Rel, ctx.ProtoLibrary),
-		Options: ctx.PluginConfig.GetOptions(),
+		Outputs: p.outputs(ctx.ProtoLibrary, mappings),
+		Options: options,
 	}
 }
 
 func (p *protocGenGrpcGatewayPlugin) shouldApply(lib protoc.ProtoLibrary) bool {
 	for _, f := range lib.Files() {
-		if f.HasServices() {
+		if appliesToFile(f) {
 			return true
 		}
 	}
 	return false
 }
 
-func (p *protocGenGrpcGatewayPlugin) outputs(rel string, lib protoc.ProtoLibrary) []string {
+func appliesToFile(f *protoc.File) bool {
+	// If this proto doesn't have any services, then skip it.
+	if f.HasServices() {
+		// Not all protos that have services support gRPC Gateway.
+		// Look to see if the proto imports "google/api/annotations.proto" as a
+		// proxy to guess whether we should apply to this file or not.
+		for _, i := range f.Imports() {
+			if i.Filename == "google/api/annotations.proto" {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (p *protocGenGrpcGatewayPlugin) outputs(lib protoc.ProtoLibrary, importMappings map[string]string) []string {
 	srcs := make([]string, 0)
 	for _, f := range lib.Files() {
-		if !f.HasServices() {
+		if !appliesToFile(f) {
 			continue
 		}
 		base := f.Name
-		if rel != "" {
-			base = path.Join(rel, base)
+		pkg := f.Package()
+		if mapping := importMappings[path.Join(f.Dir, f.Basename)]; mapping != "" {
+			base = path.Join(mapping, base)
+		} else {
+			base = path.Join(strings.ReplaceAll(pkg.Name, ".", "/"), base)
 		}
 		srcs = append(srcs, base+".pb.gw.go")
 	}
 	return srcs
+}
+
+func (p *protocGenGrpcGatewayPlugin) ResolvePluginOptions(cfg *protoc.PluginConfiguration, r *rule.Rule, from label.Label) []string {
+	return protobuf.ResolvePluginOptionsTransitive(cfg, r, from)
 }
